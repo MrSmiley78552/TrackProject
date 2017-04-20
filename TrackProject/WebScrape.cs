089ca66ee418a5b5456d893a60f3776e4af829fb
Mitchell Kollodge <mitchell.kollodge@ndsu.edu>
2017-04-20 10:48:27 -0500

    Wroking on WebScrape Class. Reads in website and stores them as .txt files. Currently trying to figure out how to extract all desired pdfs.

--- TrackProject/WebScrape.cs
+++ TrackProject/WebScrape.cs
@@ -9,24 +9,62 @@ namespace TrackProject
 {
     class WebScrape
     {
+        List<string> pdfFilePathList = new List<string>();
+
+        private string[] websites = {"http://demonstf.webs.com/results"};
+
         public WebScrape()
         {
-            string remoteUrl = "https://sites.google.com/a/fargoschools.org/girlstrack/meet-results";
-            string fileName = "fargoDavies.txt";
+            string[] websites = getAllWebsites();
+            //get files of all the websites
+            int i = 1;
+            foreach(var website in websites)
+            {
+                //gets the website and stores it in a .txt file
+                string url = website;
+                string fileName = "website" + i + ".txt";
+                string txtFilePath = @"C:\Users\Mitchell\Desktop\TrackProject\HS_WebSites\" + fileName;
+                WebClient myWC = new WebClient();
+                myWC.DownloadFile(url, txtFilePath);
+                myWC.Dispose();
+                i++;
 
-            WebClient myWebClient = new WebClient();
-            string myStringWebResource = fileName;
-            myWebClient.DownloadFile(remoteUrl, fileName);
-            Console.WriteLine("Success");
+                //searches through the .txt file for .pdf documents
+                getPDFsFromWebSite(txtFilePath);
+            }
+            //get pdf
+            int counter = pdfFilePathList.Count();
 
-            string[] lines = System.IO.File.ReadAllLines(fileName);
+        }
+
+        private void getPDFsFromWebSite(string txtFilePath)
+        {
+            int i = 1;
+            string[] lines = System.IO.File.ReadAllLines(txtFilePath);
             for (int lineNumber = 0; lineNumber < lines.Length; lineNumber++)
             {
-                if (lines[lineNumber].Contains("https:") && lines[lineNumber].Contains(".pdf"))
-                    Console.WriteLine("here");
+                if (lines[lineNumber].Contains(".pdf"))
+                {
+                    string pdfFilePath = @"C:\Users\Mitchell\Desktop\TrackProject\PDF_Results\pdfTestFile" + i + ".txt";
+                    pdfFilePathList.Add(pdfFilePath);
+                    i++;
+                }
             }
         }
+        private string[] getAllWebsites()
+        {
+            return websites;
+        }
+
+        private static void addWebsite(string newWebsite)
+        {
+
+        }
         
+        private void removeWebsite(string removableWebsite)
+        {
+
+        }
 
     }
 }
